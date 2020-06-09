6554ce663735f7ba0054b1e44a7e9c6b82e3bbdf
Maxondria <maxtayebwa@gmail.com>
2020-06-09 19:35:12 +0300

    add chars collection class

---
+++ sort/src/CharactersCollection.ts
@@ -0,0 +1,26 @@
+import Sorter from "./Sorter";
+
+export default class CharactersCollection extends Sorter {
+  constructor(public data: string) {
+    super();
+  }
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  public compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+
+  public swap(leftIndex: number, rightIndex: number): void {
+    const characters: string[] = this.data.split("");
+    const leftHand = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = leftHand;
+
+    this.data = characters.join("");
+  }
+}
